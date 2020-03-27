13c84477cfeef1046956dae1bc6f5deb9ae28d25
Alexandr Shvets <sasha.shvech2003@mail.ru>
2020-03-27 16:23:47 +0300

    reload1

--- js/game.js
+++ js/game.js
@@ -70,7 +70,7 @@ function draw() {
  if(xPos + bird.width >= pipe[i].x
  && xPos <= pipe[i].x + pipeUp.width
  && (yPos <= pipe[i].y + pipeUp.height
- || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height) {
+ || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || (yPos + bird.height) >= (cvs.height - fg.height)) {
  location.reload(); // Перезагрузка страницы
  }
 
